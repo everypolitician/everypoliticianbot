2e26d3d188c671fa29035894d6c64a37b20bb35a
Chris Mytton <chrismytton@gmail.com>
2015-10-15 10:51:12 +0100

    Switch to using git gem
    This handles checking for errors when shelling out and provides a nice
    ruby DSL for interacting with git repos.

--- lib/everypoliticianbot.rb
+++ lib/everypoliticianbot.rb
@@ -1,10 +1,9 @@
 require 'everypoliticianbot/version'
+require 'git'
 
 module Everypoliticianbot
   # Mixin to provide a GitHub client and helpers.
   module Github
-    class SystemCallFail < StandardError; end
-
     def github
       @github ||= Octokit::Client.new(access_token: ENV['GITHUB_ACCESS_TOKEN'])
     end
@@ -12,42 +11,34 @@ module Everypoliticianbot
     def with_git_repo(repo_name, options)
       repo = github.repository(repo_name)
       branch = options.fetch(:branch, 'master')
-      with_tmp_dir do |dir|
-        system("git clone --quiet #{clone_url(repo.clone_url)} #{dir}")
-        system("git checkout -B #{branch}")
+      message = options.fetch(:message)
+      with_tmp_dir do
+        git = clone(clone_url(repo.clone_url))
+        git.branch(branch).checkout
         yield
-        git_commit_and_push(options.merge(branch: branch))
+        return unless git.status.changed.any? || git.status.untracked.any?
+        git.add
+        git.commit(message)
+        git.push
+      end
+    end
+
+    def clone(url)
+      @git ||= Git.clone(url, '.').tap do |g|
+        g.config('user.name', github.login)
+        g.config('user.email', github.emails.first[:email])
       end
     end
 
     def clone_url(uri)
-      repo_clone_url = URI.parse(uri)
-      repo_clone_url.user = github.login
-      repo_clone_url.password = github.access_token
-      repo_clone_url
-    end
-
-    def with_tmp_dir(&block)
-      Dir.mktmpdir do |tmp_dir|
-        Dir.chdir(tmp_dir, &block)
+      URI.parse(uri).tap do |repo_clone_url|
+        repo_clone_url.user = github.login
+        repo_clone_url.password = github.access_token
       end
     end
 
-    def git_config
-      @git_config ||= "-c user.name='#{github.login}' " \
-        "-c user.email='#{github.emails.first[:email]}'"
-    end
-
-    def git_commit_and_push(options)
-      branch_name = options.fetch(:branch)
-      message = options.fetch(:message)
-      system('git add .')
-      system(%(git #{git_config} commit --quiet --message="#{message}" || true))
-      system("git push --quiet origin #{branch_name}")
-    end
-
-    def system(*args)
-      fail SystemCallFail, "#{args} #{$CHILD_STATUS}" unless Kernel.system(*args)
+    def with_tmp_dir(&block)
+      Dir.mktmpdir { |tmp_dir| Dir.chdir(tmp_dir, &block) }
     end
   end
 end
